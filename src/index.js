f3c55e819cbe2084c4b529e4664febc2d6ab70b7
Ash <144191279+Ash-Wayne@users.noreply.github.com>
2024-06-01 08:28:59 -0400

    Made it ready to be an npm package

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import './styles.css';
-import { addClickDropdownCapability, addHoverDropdownCapability } from './dropdown';
 
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const foodItems = document.getElementById('food-items');
-const kebabIcon = document.getElementById('kebab-icon');
-const stationary = document.getElementById('stationary');
-const meatballsIcon = document.getElementById('meatballs-icon');
-const drinks = document.getElementById('drinks');
-const fruitsHeader = document.getElementById('fruits-header');
-const fruits = document.getElementById('fruits');
+export function addClickDropdownCapability(target, contents, horizontalLocationOfContents) {
+	positionContents(contents, horizontalLocationOfContents);
 
-addClickDropdownCapability(hamburgerIcon, foodItems, { left: '-30px' });
+	let menuIsOpen = false;
 
-addHoverDropdownCapability(kebabIcon, stationary, { left: '-30px' });
+	target.addEventListener('click', e => {
+		if (!menuIsOpen) {
+			contents.classList.remove('hidden');
+			menuIsOpen = true;
+			e.stopPropagation();
+		}
+	});
 
-addClickDropdownCapability(meatballsIcon, drinks, { left: '-30px' });
+	window.addEventListener('click', () => {
+		if (menuIsOpen) {
+			contents.classList.add('hidden');
+			menuIsOpen = false;
+		}
+	});
+}
 
-addHoverDropdownCapability(fruitsHeader, fruits, { left: '-15px' });
+export function addHoverDropdownCapability(target, contents, horizontalLocationOfContents) {
+	positionContents(contents, horizontalLocationOfContents);
+
+	target.addEventListener('mouseenter', () => {
+		contents.classList.remove('hidden');
+	});
+
+	target.addEventListener('mouseleave', () => {
+		if (!contents.matches(':hover')) {
+			contents.classList.add('hidden');
+		}
+	});
+
+	contents.addEventListener('mouseleave', () => {
+		contents.classList.add('hidden');
+	});
+}
+
+function positionContents(contents, horizontalLocationOfContents) {
+	contents.classList.add('vertical-position');
+	contents.setAttribute(
+		'style',
+		`${Object.keys(horizontalLocationOfContents)[0]}: ${Object.values(horizontalLocationOfContents)[0]};`
+	);
+}
